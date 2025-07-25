8a5bc0f9889658f2b390703d0ee51c93579d7ab8
ASArutyunyan <aleks_pochta_krd@mail.ru>
2025-07-25 18:28:54 +0300

    Задание создание студента и его поиск

---
+++ src/com/studentapp/Main2.java
@@ -0,0 +1,45 @@
+package com.studentapp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class Main2 {
+    private static List<Student> studentList;
+
+    public static void main(String[] args) {
+        System.out.println("*************** Student Management System ***************");
+        System.out.println("*********************** Welcome *************************");
+        studentList = new ArrayList<Student>();
+
+        Student s1;
+        s1 = new Student("Alex ewe", 31, "S-1");
+        s1.enrollCourse("Java");
+        s1.enrollCourse("Python");
+        s1.enrollCourse("DSA");
+        System.out.println(s1);
+
+        s1.printStudentInfo();
+
+        Student s2 = new Student("Uday", 24, "S-888");
+        s2.enrollCourse("Java");
+        Student s3 = new Student("Uday", 24, "S-888");
+        s3.enrollCourse("Java");
+
+
+        studentList.add(s1);
+        studentList.add(s2);
+        studentList.add(s3);
+        System.out.println(studentList);
+
+        Student result = findStudentById("S-1777");
+        System.out.println("Результат поиска: " + result);
+    }
+
+    public static Student findStudentById(String studentId) {
+        return studentList.stream()
+                .filter(x -> x.getStudentId().equalsIgnoreCase(studentId))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Данных идентификатора студента не найдено."));
+    }
+}
---
+++ src/com/studentapp/Student.java
@@ -0,0 +1,113 @@
+package com.studentapp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Student {
+    private String name;
+    private int age;
+    private String studentId;
+    private List<String> courses;
+
+    public Student(String name, int age, String studentId) {
+        super();
+        if (validateAge(age) && validateName(name) && validateStudentId(studentId)) {
+            this.name = name;
+            this.age = age;
+            this.studentId = studentId;
+            courses = new ArrayList<>(); //инициализируем courses
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public List<String> getCourses() {
+        return courses;
+    }
+
+    public void enrollCourse(String course) {
+        if (validateCourseName(course)) {
+            if (!courses.contains(course)) {
+                courses.add(course);
+                System.out.println("Студен записан на курс: " + course + "!");
+            } else {
+                System.err.println("Студент уже добавлен на курс " + course);
+            }
+        }
+    }
+
+    public void printStudentInfo() {
+        System.out.println("=============== Student Information =====================");
+        System.out.println("Student name: " + name);
+        System.out.println("Student age: " + age);
+        System.out.println("Enrolled For: " + courses);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", studentId='" + studentId + '\'' +
+                ", courses=" + courses +
+                '}';
+    }
+
+    public boolean validateAge(int age) {
+        if (age >= 19 && age <= 35) {
+            return true;
+        } else {
+            System.err.println("Недопустимый возраст! Возраст должен быть от 19 до 35 лет.");
+            return false;
+        }
+    }
+
+    public boolean validateName(String name) {
+        //имя будет в нижним регистри: a-z, начинается с буквы: ^, если надо разный регистр то: a-zA-Z
+        //если пробелы нужны то: \\s, комбинации имени и фамилии или несколько имени: +, заканчиваем тем чем начали: $
+        String nameRegex = "^[a-zA-Z\\s]+$";
+        Pattern namePattern = Pattern.compile(nameRegex);
+        Matcher nameMatcher = namePattern.matcher(name);
+        if (nameMatcher.matches()) {
+            return true;
+        } else {
+            System.err.println("Неверное имя. Введите только буквы.");
+            return false;
+        }
+    }
+
+
+    private boolean validateStudentId(String studentId) {
+        String studentIdRegex = "S-\\d+$";  //шаблон: S - 123, \\d-это цифры
+        Pattern studentIdPattern = Pattern.compile(studentIdRegex);
+        Matcher studentIdMatcher = studentIdPattern.matcher(studentId);
+        if (studentIdMatcher.matches()) {
+            return true;
+        } else {
+            System.err.println("Не верный. Верный формат S - 123... ");
+            return false;
+        }
+    }
+
+    public boolean validateCourseName(String course) {
+        if (course.equalsIgnoreCase("Java") || course.equalsIgnoreCase("DSA")
+                || course.equalsIgnoreCase("Python")) {
+            return true;
+        } else {
+            System.err.println("Не верный курс. Выберите из списка: Java, DSA, Python");
+            return false;
+        }
+    }
+}
